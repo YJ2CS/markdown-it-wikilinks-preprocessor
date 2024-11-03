3d58a8c488dc23e825fc2c90ff1e4469c29c8abc
YJ2CS <Blabber7846@outlook.com>
2024-11-03 21:36:24 +0800

    init1

--- index.js
+++ index.js
@@ -1,84 +1,17 @@
-'use strict';
 
-const Plugin = require('markdown-it-regexp');
-const extend = require('extend');
-const sanitize = require('sanitize-filename');
+//  [[path |label]] [[wikilink | label]]  后面的 label可有可无
+const reg_wiki = /\[\[\s*([^\x00-\x1f\x7f]*?)\s*(\|\s*(.+?)\s*)?\s*\]\]/g;
 
-module.exports = (options) => {
-  const defaults = {
-    linkPattern: /\[\[([-\w\s/]+)(\|([-\w\s/]+))?\]\]/,
-    baseURL: '/',
-    relativeBaseURL: './',
-    makeAllLinksAbsolute: false,
-    uriSuffix: '.html',
-    htmlAttributes: {
-    },
-    generatePageNameFromLabel: (label) => {
-      return label;
-    },
-    postProcessPageName: (pageName) => {
-      pageName = pageName.trim();
-      pageName = pageName.split('/').map(sanitize).join('/');
-      pageName = pageName.replace(/\s+/g, '_');
-      return pageName;
-    },
-    postProcessLabel: (label) => {
-      label = label.trim();
-      return label;
-    },
-  };
 
-  options = extend(true, defaults, options);
-
-  function isAbsolute (pageName) {
-    return options.makeAllLinksAbsolute || pageName.charCodeAt(0) === 0x2F; /* / */
-  }
-
-  function removeInitialSlashes (str) {
-    return str.replace(/^\/+/g, '');
-  }
-
-  return Plugin(
-    options.linkPattern,
-    (match, utils) => {
-      let label = '';
-      let pageName = '';
-      let href = '';
-      const htmlAttrs = [];
-      let htmlAttrsString = '';
-      const isSplit = !!match[3];
-      if (isSplit) {
-        label = match[3];
-        pageName = match[1];
-      } else {
-        label = match[1];
-        pageName = options.generatePageNameFromLabel(label);
-      }
-
-      label = options.postProcessLabel(label);
-      pageName = options.postProcessPageName(pageName);
-
-      // make sure none of the values are empty
-      if (!label || !pageName) {
-        return match.input;
-      }
-
-      if (isAbsolute(pageName)) {
-        pageName = removeInitialSlashes(pageName);
-        href = options.baseURL + pageName + options.uriSuffix;
-      } else {
-        href = options.relativeBaseURL + pageName + options.uriSuffix;
-      }
-      href = utils.escape(href);
-
-      htmlAttrs.push(`href="${href}"`);
-      for (const attrName in options.htmlAttributes) {
-        const attrValue = options.htmlAttributes[attrName];
-        htmlAttrs.push(`${attrName}="${attrValue}"`);
-      }
-      htmlAttrsString = htmlAttrs.join(' ');
-
-      return `<a ${htmlAttrsString}>${label}</a>`;
-    }
-  );
+const markdownItLinkPreprocessor = (md) => {
+    // 在内容解析之前进行预处理
+    md.core.ruler.before('normalize', 'preprocess_custom_links', (state) => {
+        state.src = state.src.replace(reg_wiki, (match, href, seperator_label, label) => {
+            debugger
+            const [txt, url] = label ? [label, href] : [href, href];
+            return `[${txt.trim()}](${url.trim()})`;
+        });
+    });
 };
+
+module.exports =markdownItLinkPreprocessor;
